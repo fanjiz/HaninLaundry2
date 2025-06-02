859e52ec4f29a7f1d967c9dde6ea8d8d5cb779dc
Lenovo <Lenovo@LAPTOP-PTFERJE4>
2025-06-02 19:08:47 +0700

    LoginForm

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,11 +31,13 @@
             btnLogin = new Button();
             lblUsername = new Label();
             lblPassword = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
             SuspendLayout();
             // 
             // btnLogin
             // 
-            btnLogin.Location = new Point(463, 484);
+            btnLogin.Location = new Point(462, 327);
             btnLogin.Name = "btnLogin";
             btnLogin.Size = new Size(112, 34);
             btnLogin.TabIndex = 2;
@@ -45,6 +47,7 @@
             // lblUsername
             // 
             lblUsername.AutoSize = true;
+            lblUsername.BackColor = SystemColors.ButtonHighlight;
             lblUsername.Location = new Point(473, 132);
             lblUsername.Name = "lblUsername";
             lblUsername.Size = new Size(91, 25);
@@ -54,18 +57,36 @@
             // lblPassword
             // 
             lblPassword.AutoSize = true;
-            lblPassword.Location = new Point(486, 291);
+            lblPassword.BackColor = SystemColors.ButtonHighlight;
+            lblPassword.Location = new Point(477, 221);
             lblPassword.Name = "lblPassword";
             lblPassword.Size = new Size(87, 25);
             lblPassword.TabIndex = 4;
             lblPassword.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(407, 160);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(220, 31);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(407, 249);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(220, 31);
+            textBox2.TabIndex = 6;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
+            BackgroundImage = Properties.Resources.login;
             ClientSize = new Size(1143, 750);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(lblPassword);
             Controls.Add(lblUsername);
             Controls.Add(btnLogin);
@@ -81,5 +102,7 @@
         private Button btnLogin;
         private Label lblUsername;
         private Label lblPassword;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
