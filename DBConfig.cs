981275a1b34d59eb2509d49cea74ac66cc3b8774
fanjiz <irpanaziz232@gmail.com>
2025-06-10 01:22:39 +0700

    Database

---
+++ DBConfig.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaninLaundry
+{
+    class DBConfig
+    {
+        public static string dbServer = "localhost";
+        public static string dbUser = "root";
+        public static string dbName = "dbhanin";
+        public static string dbPass = "";
+
+        public static string ConnStr => $"server={dbServer};user={dbUser};database={dbName};password={dbPass};";
+
+    }
+}
