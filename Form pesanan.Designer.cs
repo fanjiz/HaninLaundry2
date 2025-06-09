73e0e7245c477a6c29756ee0e9f5b75db54ba45d
Lenovo <Lenovo@LAPTOP-PTFERJE4>
2025-06-09 21:35:50 +0700

    Tambah Pesanan

--- Form pesanan.Designer.cs
+++ Form pesanan.Designer.cs
@@ -28,10 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_pesanan));
             button1 = new Button();
             button2 = new Button();
             button3 = new Button();
             button4 = new Button();
+            textBox1 = new TextBox();
+            button5 = new Button();
+            button6 = new Button();
             SuspendLayout();
             // 
             // button1
@@ -71,18 +75,48 @@
             button4.UseVisualStyleBackColor = true;
             button4.Click += button4_Click;
             // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(1134, 14);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 4;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(242, 88);
+            button5.Name = "button5";
+            button5.Size = new Size(157, 34);
+            button5.TabIndex = 5;
+            button5.Text = "Tambah Pesanan";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(459, 88);
+            button6.Name = "button6";
+            button6.Size = new Size(157, 34);
+            button6.TabIndex = 6;
+            button6.Text = "Segarkan";
+            button6.UseVisualStyleBackColor = true;
+            // 
             // Form_pesanan
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1373, 781);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(textBox1);
             Controls.Add(button4);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form_pesanan";
             Text = "Form_pesanan";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -91,5 +125,8 @@
         private Button button2;
         private Button button3;
         private Button button4;
+        private TextBox textBox1;
+        private Button button5;
+        private Button button6;
     }
 }
